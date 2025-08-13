74655ee1b9f6ebecafcff0948eadfaae8c92a30d
Dell <mattewczlp@seznam.cz>
2025-08-13 22:04:13 +0200

    innit fix

--- src/__init__.py
+++ src/__init__.py
@@ -1,1 +1,8 @@
-__version__ = "1.0.0"
+import logging
+logging.getLogger(__name__).addHandler(logging.NullHandler())
+
+try:
+    from importlib.metadata import version, PackageNotFoundError
+    __version__ = version("crypto-sentiment-tracker")
+except Exception:
+    __version__ = "0.0.0"
